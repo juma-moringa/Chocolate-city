4b29028ef28969bd8f0f100a165f51f0f4d7d4d2
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-24 12:31:32 +0300

    define business methods

--- myhood/models.py
+++ myhood/models.py
@@ -9,7 +9,7 @@ class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length=100,blank=False)
     neighbourhood_location = models.CharField(max_length=100, blank=False)
     occupants_count = models.IntegerField()
-    admin = models.ForeignKey(User,related_name='Hadmin', null=True,on_delete=models.CASCADE)
+    admin = models.ForeignKey(User,related_name='Hood_admin', null=True,on_delete=models.CASCADE)
 
     #neighbourbood methods
     def __str__(self):
@@ -25,16 +25,18 @@ class Neighbourhood(models.Model):
     def find_neighbourhood(cls, neighbourhood_id):
         return cls.objects.filter(id=neighbourhood_id) 
 
+    def update_neighborhood(self):
+        neighbourhood_name = self.name
+        self.name = neighbourhood_name  
+        
     @classmethod
     def update_occupants(cls,neighbourhood_id):
         member = cls.objects.get(id=neighbourhood_id)
         new_count = member.occupants_count + 1
         cls.objects.filter(id = neighbourhood_id).update(occupants_count = new_count) 
 
-    def update_neighborhood(self):
-        neighbourhood_name = self.name
-        self.name = neighbourhood_name       
 
+# 2. Userprofile
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default='0')
     bio = models.TextField()  
@@ -44,9 +46,42 @@ class Profile(models.Model):
     blank=True, null=True)
     neighbourhood_name = models.CharField(max_length=60,blank=False)
     location = models.CharField(max_length=60,blank=False)
-      
+
+    # profile methods
+    def __str__(self):
+        return self.user
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()  
+
+# 3. Business class
 class Business(models.Model): 
     business_name= models.CharField(max_length=100, blank=False) 
     user= models.ForeignKey(Profile,related_name='business_owner',on_delete=models.CASCADE,)   
     neighbourhood_id= models.ForeignKey(Neighbourhood,related_name='business',on_delete=models.CASCADE)
     business_email = models.CharField(max_length=50,blank=False)
+    
+    # profile methods
+    def __str__(self):
+        return self.business_name
+
+    def save_business(self):
+        self.save()
+
+    def create_business(self):
+            self.save()
+
+    def delete_business(self):
+        self.delete()   
+
+    @classmethod
+    def find_business(cls,business_id):
+        business = cls.objects.get(id = business_id)
+        return business
+
+    def update_business(self):
+        business_name = self.name
+        self.name = business_name     
