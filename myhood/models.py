39203c7930f3b2c3cfe4a4f1e9889be6207c6aef
juma-moringa <allan.juma@student.moringaschool.com>
2021-07-26 20:04:37 +0300

    removed typos

--- myhood/models.py
+++ myhood/models.py
@@ -52,11 +52,7 @@ class Profile(models.Model):
         profile = Profile.objects.all()
         return profile
     
-    @receiver(post_save, sender=User)
-    def update_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-        instance.profile.save()
+    
 
     def save_profile(self):
         self.save()
@@ -115,3 +111,11 @@ class Post(models.Model):
 
     def delete_post(self):
         self.delete()        
+
+
+
+#    @receiver(post_save, sender=User)
+#     def update_user_profile(sender, instance, created, **kwargs):
+#         if created:
+#             Profile.objects.create(user=instance)
+#         instance.profile.save()     
